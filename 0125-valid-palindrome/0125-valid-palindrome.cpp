d17d83e8e2feb536b8febc02c226dce47a61c97c
Miguel Pafume <142275257+miguelpafume@users.noreply.github.com>
2025-03-26 12:13:28 -0300

    Time: 214 ms (7.72%), Space: 10.4 MB (18.46%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,10 +5,9 @@ public:
     bool isPalindrome(std::string s) {
         std::string t;
 
-        for (int i = 0, n = s.size(); i < n; i++) {
+        for (int i = 0; i < s.size(); i++) {
             if (!((s[i] >= 'a' && s[i] <= 'z' ) || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))) {
                 s.erase(s.begin()+i);
-                n--;
                 i--;
                 continue;
             }
