ce7fa42f9b40e78b933e66c793a108a3327f76fb
Miguel Pafume <142275257+miguelpafume@users.noreply.github.com>
2025-04-07 19:07:12 -0300

    Time: 9 ms (19.44%), Space: 11.8 MB (76.74%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int found = ransomNote.find_first_of(magazine.back());
+        int x = 0;
+        while (magazine.size() > 0) {
+            if (found != string::npos) {
+                ransomNote[found] = '*';
+                x++;
+            }
+            magazine.pop_back();
+            if (magazine.size() > 0) { found = ransomNote.find_first_of(magazine.back()); }
+        }
+        if (x == ransomNote.size()) {
+            return true;
+        }
+        return false;
+    }
+};
