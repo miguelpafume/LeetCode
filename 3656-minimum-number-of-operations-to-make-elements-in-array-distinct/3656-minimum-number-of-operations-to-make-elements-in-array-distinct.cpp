81eade5380b52cd3232d34e8206d9da405e42eea
Miguel Pafume <142275257+miguelpafume@users.noreply.github.com>
2025-04-08 19:07:47 -0300

    Time: 42 ms (6.2%), Space: 42.1 MB (5.51%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,22 +1,31 @@
-#include <unordered_set>
+bool containsRepeats (const unordered_map<int, int> map) {
+    for (auto& [key, count]: map) {
+        if (count > 1) return true;
+    }
+    return false;
+}
 
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        unordered_set<int> s;
-        int op = 0;
-        int i = 0;
-        while (i < nums.size()) {
-            if (s.contains(nums[i])) {
-                op++;
-                nums.erase(nums.begin(), (nums.size() >= 3 ? nums.begin()+3 : nums.end()));
-                s.clear();
-                i = 0;
-            } else {
-                s.insert(nums[i]);
-                i++;
+        unordered_map<int, int> m;
+        int operations = 0;
+
+        for (int i: nums) { m[i]++; }
+
+        if (!containsRepeats(m)) return operations;
+
+        while (!nums.empty()) {
+            if (nums.size() < 3) return operations+1;
+
+            for (int i = 0; i < 3; i++) {
+                m[nums[i]]--;
+                if (m[nums[i]] == 0) m.erase(nums[i]);
             }
+            nums.erase(nums.begin(), nums.begin()+3);
+            operations++;
+            if (!containsRepeats(m)) return operations;
         }
-        return op;
+        return operations;
     }
 };
