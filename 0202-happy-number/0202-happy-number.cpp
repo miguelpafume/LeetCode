0f45596cd7a6c0b4c634abcf988b2d2ff132d0b3
Miguel Pafume <142275257+miguelpafume@users.noreply.github.com>
2025-04-09 16:23:26 -0300

    Time: 0 ms (100%), Space: 8.4 MB (22.3%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,20 @@
+int sumNumber(int n) {
+    int x = 0;
+    while (n != 0) {
+        x += (n%10) * (n%10);
+        n /= 10;
+    }
+    return x;
+}
+
 class Solution {
 public:
     bool isHappy(int n) {
         unordered_set<int> s;
-        queue<int> q;
-        while (!s.contains(n)) {
+
+        while (n != 1 && !s.contains(n)) {
             s.insert(n);
-            
-            while (n > 0) {
-                q.push(n%10);
-                n /= 10;
-            }
-            
-            while (q.size() > 0) {
-                n += pow(q.front(), 2);
-                q.pop();                
-            }
+            n = sumNumber(n);
         }
         return (n == 1 ? true : false);
     }
