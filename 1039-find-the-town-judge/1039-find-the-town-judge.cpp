7721f742a1635e48f421a50cd9f2a4b501fdaa27
Miguel Pafume <142275257+miguelpafume@users.noreply.github.com>
2025-04-25 20:13:05 -0300

    Time: 7 ms (51.78%), Space: 65.8 MB (53.57%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -3,17 +3,15 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if (n == 1) return 1;
 
-        unordered_map<int, bool> m1;
-        unordered_map<int, int> m2;
+        unordered_map<int, int> m;
 
         for (vector<int>& vec: trust) {
-            m1[vec[0]] = true;
-            if (m1.find(vec[1]) == m1.end()) m1[vec[1]] = false;
-            m2[vec[1]]++;
+            m[vec[0]]--;
+            m[vec[1]]++;
         }
 
-        for (auto& [key, value]: m1) {
-            if(!value && m2[key] == n-1) {
+        for (auto& [key, value]: m) {
+            if(value == n-1) {
                 return key;
             }
         }
