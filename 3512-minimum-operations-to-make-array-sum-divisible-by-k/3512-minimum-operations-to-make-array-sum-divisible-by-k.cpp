cb6726e47e11b8a3214f254619f29366ce877f3c
Miguel Pafume <142275257+miguelpafume@users.noreply.github.com>
2026-02-08 13:03:30 -0300

    Time: 0 ms (100%), Space: 45.4 MB (32.83%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    int sumVec(vector<int> &nums) {
+    int minOperations(vector<int>& nums, int k) {
         int sum = 0;
         for(int &i: nums) {
             sum += i;
         }
-        return sum;
-    }
-
-    int minOperations(vector<int>& nums, int k) {
-        return sumVec(nums) % k;
+        
+        return sum % k;
     }
 };
