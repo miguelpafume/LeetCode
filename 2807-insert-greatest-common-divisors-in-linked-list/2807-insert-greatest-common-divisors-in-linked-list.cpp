60532b22a71cc0c98cbfae8b8eccb6543a0139c3
Miguel Pafume <142275257+miguelpafume@users.noreply.github.com>
2026-02-11 10:23:08 -0300

    Time: 20 ms (9.6%), Space: 36.1 MB (15.65%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -13,13 +13,25 @@
 
 class Solution {
 public:
+    int myGcd (const int &x, const int &y) {
+        if (!x || !y) return 0;
+                
+        int min = (x < y) ? x : y;
+
+        for (int i = min; i > 0; i--) {
+            if (x % i == 0 && y % i == 0) return i;
+        }
+
+        return 1;
+    }
+
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
         if (head->next == nullptr) return head;
 
         ListNode *currentNode = head;
 
         while (currentNode->next != nullptr) {
-            ListNode *newNode = new ListNode(std::gcd(currentNode->val, currentNode->next->val), currentNode->next);
+            ListNode *newNode = new ListNode(myGcd(currentNode->val, currentNode->next->val), currentNode->next);
             currentNode->next = newNode;
             currentNode = newNode->next;
         }
